f485aa7967b57742249f4749b1202866556a6331
Diana Kdyrova <dianablaenec@gmail.com>
2024-03-28 18:43:49 +0200

    added new iles

---
+++ lessons/cw6.py
@@ -0,0 +1,88 @@
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9]
+# even_numbers = list()
+# for number in numbers:
+#     if number % 2 == 0:
+#         even_numbers.append(number)
+#     else:
+#         print(f'{number} is not even number')
+# print(even_numbers)
+
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9]
+# even_numbers = list()
+# for number in numbers:
+#     if number % 2 == 0:
+#         even_numbers.append(number)
+# print(even_numbers)
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9]
+# even_numbers = list()
+# for number in numbers:
+#     if number == 100:
+#         break
+#     print(f'My number {number})
+
+# numbers = [1, 3, 5, 10, 15, 24, 56, 63, 99, 120, 159, 666]
+#
+# my_number = int(input("Enter number: "))
+#
+# even_numbers = list()
+# for number in numbers:
+#     if number % my_number == 0:
+#         even_numbers.append(number)
+# print(even_numbers)
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9]
+# even_numbers = list()
+# for number in numbers:
+#     if number == 100:
+#         break   # закончется итерация
+# print(f'My number {number}')
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9, 1]  # исключаем не уникальные данные
+# for number in numbers:
+#     if numbers.count(number) == 1:
+#         print(f'Number is unique {number}')
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9, 1]  # исключаем не уникальные данные
+# unique_elements = []
+# for number in numbers:
+#     if numbers.count(number) == 1:
+#         unique_elements.append(number)
+# print(unique_elements)
+
+# numbers = [1, 10, 100, 200, 300, 5, 4, 5, 7, 9, 1]  # исключаем не уникальные данные
+# unique_elements = []
+# for number in numbers:
+#     if numbers.count(number) != 1:  # True
+#         print("List contains not unique elements")
+#         break
+
+# numbers = [1, 10, 100]  # исключаем не уникальные данные
+# unique_elements = []
+# for number in numbers:
+#     if numbers.count(number) != 1:
+#         print("List contains not unique elements")
+#         break
+# else:
+#     print("List contain only unique elements")
+
+
+# Task 3
+# *
+# * *
+# * * *
+# * * * *
+
+# for i in range(5):
+#     print('* ' * (i + 1))
+
+# numbers = 1, 2, 3, 4, 5
+# unique_elements =
+# print(type(unique_elements))
+# for number in numbers:
+#     if numbers.count(number) == 1:
+#         unique_elements.append(number)1,2,
+#         print(unique_elements)
+
+
