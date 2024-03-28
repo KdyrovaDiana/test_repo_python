b153e95ef0e5f23ea42176aa677f2fbc0652ed3d
Diana Kdyrova <dianablaenec@gmail.com>
2024-03-28 20:20:35 +0200

    added new file division_without_remainder.py

---
+++ Home work 6/division_without_remainder.py
@@ -0,0 +1,9 @@
+numbers = [1, 4, 6, 10, 25, 29, 33, 65, 68, 99, 103, 120, 125, 159]
+even_numbers = list()
+my_number = int(input("Enter number: "))
+for number in numbers:
+    if number % my_number == 0:
+        even_numbers.append(number)
+print(even_numbers)
+
+
