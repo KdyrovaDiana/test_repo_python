0a9f40db0773aa8b32c66e83e1c9ffb43ec1c992
Diana Kdyrova <dianablaenec@gmail.com>
2024-04-04 17:40:35 +0300

    added corrected

--- Home work seven/hw1_unique_elements.py
+++ Home work seven/hw1_unique_elements.py
@@ -1,9 +1,21 @@
+# 1- Version
 first = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+my_list = []
+for i in first:
+    if i in my_list:
+        continue
+    for j in second:
+        if i == j:
+            my_list.append(i)
+            break
+print(f'My list of unique elements {my_list}. 1-Version')
 
+# 2-Version
+first = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+my_set_1 = set(first)
+my_set_2 = set(second)
+my_list = list(my_set_1.intersection(my_set_2))
+print(f'My list of unique elements {my_list}. 2-Version')
 
-my_list = list(first) + list(second)
-my_set = set(my_list)
-my_set_l = list(set(my_list))
-my_set_1 = my_set_l.sort()
-print(f'My list of unique elements {my_set_l}')
