bcf4086348052daebac0696d0b412483bfe9b1f9
Diana Kdyrova <dianablaenec@gmail.com>
2024-04-19 22:34:21 +0300

    added amendments

--- Home work 9/new_file_txt.py
+++ Home work 9/new_file_txt.py
@@ -2,4 +2,3 @@ with open('new_text.txt', 'r', encoding='utf-8') as file:
     with open('new_2.txt', 'w', encoding='utf-8') as new_file:
         for line in file:
             new_file.write(line)
-file.close()
