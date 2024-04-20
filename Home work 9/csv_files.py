90b1ec0fde8a75c29e7f02aa6c38fd7ba02c70c1
Diana Kdyrova <dianablaenec@gmail.com>
2024-04-20 17:48:58 +0300

    added new file/files csv

---
+++ Home work 9/csv_files.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('names_4.csv', 'w', newline='') as new_file:
+        fieldnames = ['email']
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+
+        csv_writer.writeheader()
+
+        for elements in csv_reader:
+            del elements['last_name']
+            del elements['first_name']
+            csv_writer.writerow(elements)
