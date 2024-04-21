bccf897ca4f4ae232468e0a648225cba5ac2ea18
Diana Kdyrova <dianablaenec@gmail.com>
2024-04-21 15:23:05 +0300

    added new file user_key.py

---
+++ Home work 9/user_key.py
@@ -0,0 +1,12 @@
+course = {'title': 'AQA', 'language': 'Python', 'level': 'PRO', 'frame': 'pytest'}
+key = (input("Enter the key: "))
+key2 = key.lower()
+
+if key2 in course:
+    try:
+        user = course.get(key2)
+        print(user)
+    except KeyError:
+        user = course.get(key2)
+else:
+    print(f'Oops! Incorrect key entered.')
