ae83c286839e4f7bfc1d35d8829cdb08316539a6
Diana Kdyrova <dianablaenec@gmail.com>
2024-04-27 18:36:09 +0300

    added amendments

--- hw10/fyn_calc.py
+++ hw10/fyn_calc.py
@@ -1,32 +1,16 @@
-def summa(number1, number2):
-    return number1 + number2
-
-
-def subtract(number1, number2):
-    return number1 - number2
-
-
-def multiply(number1, number2):
-    return number1 * number2
-
-
-def divide(number1, number2):
-    return number1 / number2
-
-
 def calc(number1, number2, operator):
     result = None
     if operator == '+':
-        result = summa(number1, number2)
+        result = number1 + number2
     elif operator == '-':
-        result = subtract(number1, number2)
+        result = number1 - number2
     elif operator == '*':
-        result = multiply(number1, number2)
+        result = number1 * number2
     elif operator == '/':
         if number2 == 0:
             print("Stop! You can't divide by zero")
             return
-        result = divide(number1, number2)
+        result = number1 / number2
     else:
         print("Incorrect operation")
     return result
