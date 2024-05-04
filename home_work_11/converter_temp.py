291639c6ddd6edf96d4f28de880d45d3bb01ed54
Diana Kdyrova <dianablaenec@gmail.com>
2024-05-05 02:39:26 +0300

    added

--- home_work_11/converter_temp.py
+++
@@ -1,7 +0,0 @@
-from home_work_11.mathematics.calculator.calculator import run_convert_t
-
-if __name__ == '__main__':
-    print('''Welcome to the currency converter! \n Select operation: \n    - Fahrenheit enter: f 
-    - Celsius enter: c \n  ''')
-    choice = input("What do you want to chose? ").lower()
-    run_convert_t(choice)
