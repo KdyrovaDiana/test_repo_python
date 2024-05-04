cb949d378fd7eb3ce1ff0f0f05d8dea37ff6a62f
Diana Kdyrova <dianablaenec@gmail.com>
2024-05-05 02:16:01 +0300

    added

--- home_work_11/mathematics/calculator/correct_symbols/correct_symbols.py
+++
@@ -1,4 +0,0 @@
-def check_user_choice(operation, correct_symbols=('f', 'c')):
-    while operation not in correct_symbols:
-        operation = input("Enter operation (Enter 'f', 'c'): ")
-    return operation
